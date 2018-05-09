610b31e29e31094e555b5fb611e9890157128b69
dnewh <dnewhall@fishbowlsolutions.com>
2018-05-09 16:16:36 -0500

    oops

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import * as firebase from 'firebase';
+import config from './config/firebase.json';
 
-const config = {
-    apiKey: "AIzaSyCWACpuXsd3z8429KkvK5FNWU6tVRh7mOo",
-    authDomain: "fishbowl-onboarding-demo.firebaseapp.com",
-    databaseURL: "https://fishbowl-onboarding-demo.firebaseio.com",
-    projectId: "fishbowl-onboarding-demo",
-    storageBucket: "fishbowl-onboarding-demo.appspot.com",
-    messagingSenderId: "133493972520"
-};
 firebase.initializeApp(config);
 
 const provider = new firebase.auth.GoogleAuthProvider();
